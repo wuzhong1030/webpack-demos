97344711de25e4996889e65be5dee860fef97593
吴忠 <wuzhong1598@qq.com>
2018-10-09 15:48:08 +0800

    add code

---
+++ webpack-demo2/build/webpack.dev.conf.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+
+module.exports = {
+    devtool: 'cheap-module-source-map',
+    devServer: {
+        port: 9091,
+        overlay: true,
+        hot: true, //启用webpack的热模块更换功能
+        hotOnly: true, //某些模块不支持热更新的时候，也不刷新页面作为回退机制，会在控制台输出热更新失败
+        proxy: {
+            '/': {
+                target: 'https://m.weibo.cn',
+                changeOrigin: true,
+                logLevel: 'debug',
+                pathRewrite: {
+                    '^/config': '/api/config'
+                },
+                header: {
+                    'Cookie': ''
+                }
+            }
+        }
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
