c499aacdbedc17b0d58314647eb7892c290d55e5
吴忠 <wuzhong1598@qq.com>
2018-10-09 15:31:50 +0800

    commit code

--- webpack-demo2/build/webpack.common.conf.js
+++ webpack-demo2/build/webpack.common.conf.js
@@ -2,20 +2,71 @@ const developmentConfig = require('./webpack.dev.conf')
 const productionConfig = require('./webpack.prod.conf')
 
 const merge = require('webpack-merge')
+const webpack = require('webpack')
+const extractTextWebpackPlugin = require('extract-text-webpack-plugin')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 const generateConfig = env => {
-    
+
+    const extractLess = new extractTextWebpackPlugin({
+        filename: 'css/[name].bundle.[hash:6].css'
+    })
+
+    const cssLoaders = [
+        {
+            loader: 'css-loader',
+            options: {
+                sourceMap: env === 'development'
+            }
+        },
+        {
+            loader: 'postcss-loader',
+            options: {
+                ident: 'postcss',
+                plugins: [
+                    require('postcss-cssnext')()
+                ].concat(
+                    env === 'production'
+                        ? require('postcss-sprites')({
+                            spritePath: 'dist/assets/images/sprites',
+                            retina: true
+                        })
+                        : []
+                )
+            }
+        },
+        {
+            loader: 'less-loader',
+            options: {
+                sourceMap: env === 'development'
+            }
+        }
+    ]
+
     const scriptLoader = [
         {
             loader: 'babel-loader'
         }
-    ].concat()
-
+    ].concat(env === 'production'
+        ? []
+        : [{
+            loader: 'eslint-loader',
+            options: {
+                formatter: require('eslint-friendly-formatter')
+            }
+        }]
+    )
 
     const styleLoader = [
-
+        env === 'production'
+            ? extractLess.extract({
+                fallback: 'style-loader',
+                use: cssLoaders
+            })
+            : [{
+                loader: 'style-loader'
+            }].concat(cssLoaders)
     ]
-
 
     return {
         entry: {
@@ -23,7 +74,34 @@ const generateConfig = env => {
         },
         output: {
             filename: 'bundle.[hash:5].js'
-        }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.less$/,
+                    use: styleLoader
+                },
+                {
+                    test: /\.js/,
+                    use: scriptLoader
+                }
+            ]
+        },
+        plugins: [
+            extractLess,
+
+            new htmlWebpackPlugin({
+                template: 'index.html',
+                filename: 'index.html',
+                minify: {
+                    collapseWhitespace: true
+                }
+            }),
+
+            new webpack.ProvidePlugin({
+                $: 'jquery'
+            })
+        ]
     }
 }
 
