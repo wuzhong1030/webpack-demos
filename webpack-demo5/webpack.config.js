87e8fa2803cb8f3c8a0a5b0bfac92c74c5e70afe
吴忠 <wuzhong1598@qq.com>
2018-10-10 17:36:43 +0800

    commit code

---
+++ webpack-demo5/webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: {
+        main: './src/foo.js',
+        vendor: ['react']
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    name: "vendor",
+                    minChunks: Infinity
+                },
+            },
+        },
+        runtimeChunk: {
+            name: "manifest",
+        }
+    },
+
+    plugins: [
+        // new webpack.optimize.SplitChunksPlugin({
+        //     name: 'vendor',
+        //     minChunks: Infinity
+        // })
+
+        new webpack.optimize.CommonsChunkPlugin({}) //webpack3
+    ]
+}
