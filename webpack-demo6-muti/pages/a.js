25419cc8a106d381aa40ceb1a94c53a317bc1324
吴忠 <wuzhong1598@qq.com>
2018-10-11 11:01:44 +0800

    add demo

---
+++ webpack-demo6-muti/pages/a.js
@@ -0,0 +1,3 @@
+import react from 'react'
+
+console.log('this is A')
---
+++ webpack-demo6-muti/pages/c.js
@@ -0,0 +1,6 @@
+import react from 'react'
+import moduleC from './components/module'
+
+console.log('this is C ')
+
+console.log(moduleC)
